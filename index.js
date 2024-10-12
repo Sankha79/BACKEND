be802342962d6ba92e08f35808b1543cc705a6b6
Sankha79 <113374276+Sankha79@users.noreply.github.com>
2024-10-13 02:49:06 +0530

    githubData commit

--- index.js
+++ index.js
@@ -4,6 +4,56 @@ require("dotenv").config();
 const app = express();
 const port = 4000;
 
+const githubData = {
+  page: 1,
+  per_page: 6,
+  total: 12,
+  total_pages: 14,
+  data: [
+    {
+      id: 1,
+      name: "cerulean",
+      year: 2000,
+      color: "#98B2D1",
+      pantone_value: "15-4020",
+    },
+    {
+      id: 2,
+      name: "fuchsia rose",
+      year: 2001,
+      color: "#C74375",
+      pantone_value: "17-2031",
+    },
+    {
+      id: 3,
+      name: "true red",
+      year: 2002,
+      color: "#BF1932",
+      pantone_value: "19-1664",
+    },
+    {
+      id: 4,
+      name: "aqua sky",
+      year: 2003,
+      color: "#7BC4C4",
+      pantone_value: "14-4811",
+    },
+    {
+      id: 5,
+      year: 2004,
+      color: "#E2583E",
+      pantone_value: "17-1456",
+    },
+    {
+      id: 6,
+      name: "blue turquoise",
+      year: 2005,
+      color: "#53B0AE",
+      pantone_value: "15-5217",
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
@@ -14,6 +64,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.send("<h1>Please login in chai aur code</h1>");
+});
+
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
 
 app.listen(process.env.PORT, () => {
